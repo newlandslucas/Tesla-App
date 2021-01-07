d4c491065d06a1b2cd7fbf6d9d576a1e383597d5
newlandslucas <lucasnewlands.m@gmail.com>
2021-01-07 17:38:04 -0300

    Car Item component

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <CarItem />
+     
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +23,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
