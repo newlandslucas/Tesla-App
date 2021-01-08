5f7303264fec040a3d05817a6e00f213beb91c20
newlandslucas <lucasnewlands.m@gmail.com>
2021-01-07 22:55:03 -0300

    Final Version

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
 
+import StyledButton from '../StyledButton';
 import styles from './styles'
 
-const CarItem = () => {
+const CarItem = (props) => {
+
+  const{ name, tagline, taglineCTA, image } = props.car;
+
   return(
     <View style={styles.carContainer}>
 
-    <ImageBackground source={require('../../assets/images/ModelS.jpeg')} 
+    <ImageBackground 
+    source={image} 
     style={styles.image}
-    
     />
 
       <View style={styles.titles}>
-        <Text style={styles.title}>Moldel S</Text>
-        <Text style={styles.subTitle}>Starting at $69.420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subTitle}>
+          {tagline}
+          {' '}
+          <Text style={styles.subtitleCTA}>
+            {taglineCTA}
+          </Text>
+        </Text>
       </View>
+
+      <View style={styles.buttonsContainer}>
+      <StyledButton 
+      type="primary" 
+      content={"Custom Order"} 
+      onPress={() => {console.warn('Custom Order was pressed');
+      }}
+      />
+
+      <StyledButton 
+      type="secondary" 
+      content={"Existing Inventory"} 
+      onPress={() => {console.warn('Existing Inventory was pressed');
+      }}
+      />
+      </View>
+
+      
   </View>
   )
 }
